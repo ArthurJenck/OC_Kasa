1526c88449213d531449c28d140a3aa59760fc72
Arthur Jenck <ajenck419@gmail.com>
2024-08-08 20:07:10 +0200

    feat: added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </Router>
+    </StrictMode>
+);
