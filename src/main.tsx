12aafcab97699e4957afb623d977f8b992d5a992
Arthur Jenck <ajenck419@gmail.com>
2024-08-20 19:56:46 +0200

    fix: attempt to fix github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./styles/main.scss"
 import Header from "./components/Header.tsx"
 import Home from "./pages/Home.tsx"
@@ -12,7 +12,7 @@ import logements from "./data/logements.json"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Router>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/OC_Kasa/"}>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -32,6 +32,6 @@ createRoot(document.getElementById("root")!).render(
                 })}
             </Routes>
             <Footer />
-        </Router>
+        </BrowserRouter>
     </StrictMode>
 )
